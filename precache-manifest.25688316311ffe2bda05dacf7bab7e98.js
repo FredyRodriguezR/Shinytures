f76cbad8d62e8448b58e35cfd76d7db55df3cace
Fredy <megablastoise94@gmail.com>
2020-10-08 18:18:15 -0500

    Updates

---
+++ precache-manifest.25688316311ffe2bda05dacf7bab7e98.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f7aec496dbc8ceda011f8ab7e675f1",
+    "url": "/Shinytures/index.html"
+  },
+  {
+    "revision": "274594f42b4da06cfea7",
+    "url": "/Shinytures/static/css/main.926ee845.chunk.css"
+  },
+  {
+    "revision": "2882a0ebc350d07b8bf6",
+    "url": "/Shinytures/static/js/2.13337f06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shinytures/static/js/2.13337f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "274594f42b4da06cfea7",
+    "url": "/Shinytures/static/js/main.830b7117.chunk.js"
+  },
+  {
+    "revision": "eb1c4abb19dc5515dc90",
+    "url": "/Shinytures/static/js/runtime-main.293480be.js"
+  },
+  {
+    "revision": "51b73d7f2f919b38269f4ac3c4e5100f",
+    "url": "/Shinytures/static/media/BungeeLayers-Inline.51b73d7f.otf"
+  },
+  {
+    "revision": "9b64cf9623500bf81e4806fbee121687",
+    "url": "/Shinytures/static/media/BungeeLayers-Regular.9b64cf96.otf"
+  },
+  {
+    "revision": "b5098bdc9dbca453e84481d78ff45085",
+    "url": "/Shinytures/static/media/Hamburger_icon.b5098bdc.svg"
+  },
+  {
+    "revision": "976f7be5233189c0ef9a7f39dcedf651",
+    "url": "/Shinytures/static/media/Logotipo_shinytures.976f7be5.png"
+  },
+  {
+    "revision": "2a810a631c1fa4b851d377e08c5e3782",
+    "url": "/Shinytures/static/media/Original.2a810a63.png"
+  },
+  {
+    "revision": "17cf745ba1f8f2fad2aa863088eed5bd",
+    "url": "/Shinytures/static/media/Shinitures_logo_final_sbx.17cf745b.png"
+  },
+  {
+    "revision": "3482b29327983cab65b945da9372dd98",
+    "url": "/Shinytures/static/media/Shinytures.3482b293.mp4"
+  },
+  {
+    "revision": "ea1b815ecbbd285726fd6cd2f487edc4",
+    "url": "/Shinytures/static/media/facebook.ea1b815e.svg"
+  },
+  {
+    "revision": "750dac63675afe3a04bd74fa0a8a1bee",
+    "url": "/Shinytures/static/media/github.750dac63.svg"
+  },
+  {
+    "revision": "60d166b38db33f894bf7555a199ca010",
+    "url": "/Shinytures/static/media/instagram.60d166b3.svg"
+  },
+  {
+    "revision": "e966da8dab2b41d26302fbbc80f03e1c",
+    "url": "/Shinytures/static/media/linkedin-logo.e966da8d.svg"
+  },
+  {
+    "revision": "509a4fbc4637f8422d427de48ac8be23",
+    "url": "/Shinytures/static/media/logoLetra.509a4fbc.png"
+  },
+  {
+    "revision": "f88e340e05dde89154c8e859d1a253cf",
+    "url": "/Shinytures/static/media/logoSteam.f88e340e.png"
+  },
+  {
+    "revision": "0a249facaf641ac1f4955ec2763a8491",
+    "url": "/Shinytures/static/media/mechanic1.0a249fac.png"
+  },
+  {
+    "revision": "394b6b0fb8317b6fb4bfa8fe36f32041",
+    "url": "/Shinytures/static/media/mechanic1.394b6b0f.mp4"
+  },
+  {
+    "revision": "4c58f84622015304b544d8694cb7a696",
+    "url": "/Shinytures/static/media/mechanic2.4c58f846.mp4"
+  },
+  {
+    "revision": "ac6e9cdd48e2432bc08e092c7809a8ef",
+    "url": "/Shinytures/static/media/twitter.ac6e9cdd.svg"
+  },
+  {
+    "revision": "64dcce6a378676ba0eed995ebc590b92",
+    "url": "/Shinytures/static/media/website-optimization.64dcce6a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Shinytures/precache-manifest.4bed5827ebb6bed4e70fc1d3b6e1867d.js"
+  "/Shinytures/precache-manifest.25688316311ffe2bda05dacf7bab7e98.js"
 );
 
 self.addEventListener('message', (event) => {
